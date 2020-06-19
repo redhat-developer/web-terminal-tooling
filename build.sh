591abb5659708033fa4b79fa09ade7f9bf042fbe
Angel Misevski <amisevsk@redhat.com>
2020-06-18 22:18:05 -0400

    Update build scripts and dockerfile to use lookaside cache
    Signed-off-by: Angel Misevski <amisevsk@redhat.com>

--- build.sh
+++ build.sh
@@ -1,13 +1,23 @@
-#!/bin/sh
+#!/bin/bash
 set -e
+
+updateBinariesTgz="false"
+while [[ "$#" -gt 0 ]]; do
+  case $1 in
+    '-u'|'--update-sources') updateBinariesTgz="true"; shift 0;;
+  esac
+  shift 1
+done
 
 TOOL=${TOOL:-podman}
 MODE=${MODE:-local}
 WEB_TERMINAL_TOOLING_IMG=${WEB_TERMINAL_TOOLING_IMG:-web-terminal-tooling:local}
 
-./download-dependencies.sh
+if [[ ! -f "container-root.tgz" ]] || [[ "$updateBinariesTgz" == "true" ]]; then
+  echo "Updating locally cached sources tarball"
+  ./get-sources-jenkins.sh
+fi
 
-cp template.Dockerfile complete.Dockerfile
-sed -i "s/\#@${MODE} /#@${MODE}\n/" complete.Dockerfile
+sed "s/\#@${MODE} /#@${MODE}\n/" template.Dockerfile > complete.Dockerfile
 
-${TOOL} build -t ${WEB_TERMINAL_TOOLING_IMG} --file complete.Dockerfile .
+${TOOL} build -t "${WEB_TERMINAL_TOOLING_IMG}" --file complete.Dockerfile .
