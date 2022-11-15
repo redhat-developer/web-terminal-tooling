cc2634388111ee55f98ee7f4be1f8570e96b9111
Angel Misevski <amisevsk@redhat.com>
2022-11-15 16:01:51 -0500

    Fix shellcheck warnings in get-sources.sh
    Signed-off-by: Angel Misevski <amisevsk@redhat.com>

--- get-sources.sh
+++ get-sources.sh
@@ -39,47 +39,47 @@ cd "$TMPDIR"
 
 echo "Downloading oc ${OC_VER} and the corresponding kubectl"
 curl -sSfL --insecure --remote-name-all \
-  ${OPENSHIFT_CLIENTS_URL}/ocp/${OC_VER}/sha256sum.txt \
-  ${OPENSHIFT_CLIENTS_URL}/ocp/${OC_VER}/openshift-client-linux-${OC_VER}.tar.gz
-echo "$(grep openshift-client-linux-${OC_VER}.tar.gz sha256sum.txt | cut -d' ' -f1) openshift-client-linux-${OC_VER}.tar.gz" | sha256sum --check --status
-tar xzf openshift-client-linux-${OC_VER}.tar.gz -C "$CONTAINER_USR_BIN_DIR" oc kubectl
+  "${OPENSHIFT_CLIENTS_URL}/ocp/${OC_VER}/sha256sum.txt" \
+  "${OPENSHIFT_CLIENTS_URL}/ocp/${OC_VER}/openshift-client-linux-${OC_VER}.tar.gz"
+echo "$(grep "openshift-client-linux-${OC_VER}.tar.gz" sha256sum.txt | cut -d' ' -f1) openshift-client-linux-${OC_VER}.tar.gz" | sha256sum --check --status
+tar xzf "openshift-client-linux-${OC_VER}.tar.gz" -C "$CONTAINER_USR_BIN_DIR" oc kubectl
 
-KUBECTL_V=$($CONTAINER_USR_BIN_DIR/kubectl version --client=true -o=json | jq -r '.clientVersion.gitVersion')
+KUBECTL_V=$("$CONTAINER_USR_BIN_DIR"/kubectl version --client=true -o=json | jq -r '.clientVersion.gitVersion')
 # Kubectl version has vMajor.Manor.BugFix-Build-GitRevision, like v1.20.1-5-g76a04fc
 # Cut build number and git revision
-KUBECTL_VER=${KUBECTL_V%-*-*}
+KUBECTL_VER="${KUBECTL_V%-*-*}"
 echo "Extracted kubectl ${KUBECTL_VER}"
 
 rm -rf "${TMPDIR:?}"/*
 
 echo "Downloading helm ${HELM_VER}"
 curl -sSfL --insecure --remote-name-all \
-  ${OPENSHIFT_CLIENTS_URL}/helm/${HELM_VER}/sha256sum.txt \
-  ${OPENSHIFT_CLIENTS_URL}/helm/${HELM_VER}/helm-linux-amd64
+  "${OPENSHIFT_CLIENTS_URL}/helm/${HELM_VER}/sha256sum.txt" \
+  "${OPENSHIFT_CLIENTS_URL}/helm/${HELM_VER}/helm-linux-amd64"
 echo "$(grep helm-linux-amd64$ sha256sum.txt | cut -d' ' -f1) helm-linux-amd64" | sha256sum --check --status
 mv helm-linux-amd64 "$CONTAINER_USR_BIN_DIR/helm"
 rm -rf "${TMPDIR:?}"/*
 
 echo "Downloading odo ${ODO_VER}"
 curl -sSfL --insecure --remote-name-all \
-  ${OPENSHIFT_CLIENTS_URL}/odo/${ODO_VER}/sha256sum.txt \
-  ${OPENSHIFT_CLIENTS_URL}/odo/${ODO_VER}/odo-linux-amd64.tar.gz
+  "${OPENSHIFT_CLIENTS_URL}/odo/${ODO_VER}/sha256sum.txt" \
+  "${OPENSHIFT_CLIENTS_URL}/odo/${ODO_VER}/odo-linux-amd64.tar.gz"
 echo "$(grep odo-linux-amd64.tar.gz sha256sum.txt | cut -d' ' -f1) odo-linux-amd64.tar.gz" | sha256sum --check --status
 tar xzf odo-linux-amd64.tar.gz -C "$CONTAINER_USR_BIN_DIR" odo
 rm -rf "${TMPDIR:?}"/*
 
 echo "Downloading tekton ${TKN_VER}"
 curl -sSfL --insecure --remote-name-all \
-  ${OPENSHIFT_CLIENTS_URL}/pipeline/${TKN_VER}/sha256sum.txt \
-  ${OPENSHIFT_CLIENTS_URL}/pipeline/${TKN_VER}/tkn-linux-amd64.tar.gz
+  "${OPENSHIFT_CLIENTS_URL}/pipeline/${TKN_VER}/sha256sum.txt" \
+  "${OPENSHIFT_CLIENTS_URL}/pipeline/${TKN_VER}/tkn-linux-amd64.tar.gz"
 echo "$(grep tkn-linux-amd64.tar.gz sha256sum.txt | cut -d' ' -f1) tkn-linux-amd64.tar.gz" | sha256sum --check --status
 tar xzf tkn-linux-amd64.tar.gz -C "$CONTAINER_USR_BIN_DIR" tkn
 rm -rf "${TMPDIR:?}"/*
 
 echo "Downloading knative ${KN_VER}"
 curl -sSfL --insecure --remote-name-all \
-  ${OPENSHIFT_CLIENTS_URL}/serverless/${KN_VER}/sha256sum.txt \
-  ${OPENSHIFT_CLIENTS_URL}/serverless/${KN_VER}/kn-linux-amd64.tar.gz
+  "${OPENSHIFT_CLIENTS_URL}/serverless/${KN_VER}/sha256sum.txt" \
+  "${OPENSHIFT_CLIENTS_URL}/serverless/${KN_VER}/kn-linux-amd64.tar.gz"
 echo "$(grep kn-linux-amd64.tar.gz sha256sum.txt | cut -d' ' -f1) kn-linux-amd64.tar.gz" | sha256sum --check --status && \
 tar xzf kn-linux-amd64.tar.gz -C "$CONTAINER_USR_BIN_DIR" kn-linux-amd64
 mv "$CONTAINER_USR_BIN_DIR/kn-linux-amd64" "$CONTAINER_USR_BIN_DIR/kn"
@@ -87,14 +87,14 @@ rm -rf "${TMPDIR:?}"/*
 
 echo "Downloading rhoas ${RHOAS_VERSION}"
 mkdir -p "$CONTAINER_OPT_DIR/rhoas"
-wget -q -O- https://github.com/redhat-developer/app-services-cli/releases/download/v${RHOAS_VERSION}/rhoas_${RHOAS_VERSION}_linux_amd64.tar.gz | \
+wget -q -O- "https://github.com/redhat-developer/app-services-cli/releases/download/v${RHOAS_VERSION}/rhoas_${RHOAS_VERSION}_linux_amd64.tar.gz" | \
   tar xz --strip-components=1 -C "$CONTAINER_OPT_DIR/rhoas"
 rm -rf "${TMPDIR:?}"/*
 chmod -R +x "${CONTAINER_USR_BIN_DIR}"
 
 echo "Downloading submariner ${SUBMARINER_VERSION}"
 mkdir -p "$CONTAINER_OPT_DIR/submariner"
-wget -q -O- https://github.com/submariner-io/releases/releases/download/v${SUBMARINER_VERSION}/subctl-v${SUBMARINER_VERSION}-linux-amd64.tar.xz | \
+wget -q -O- "https://github.com/submariner-io/releases/releases/download/v${SUBMARINER_VERSION}/subctl-v${SUBMARINER_VERSION}-linux-amd64.tar.xz" | \
   tar xJ --strip-components=1 -C "$CONTAINER_OPT_DIR/submariner"
 mv "$CONTAINER_OPT_DIR"/submariner/subctl* "$CONTAINER_OPT_DIR"/submariner/subctl
 rm -rf "${TMPDIR:?}"/*
@@ -102,14 +102,14 @@ chmod -R +x "${CONTAINER_USR_BIN_DIR}"
 
 echo "Downloading kubevirt ${KUBEVIRT_VERSION}"
 mkdir -p "$CONTAINER_OPT_DIR/kubevirt/"
-wget -q -O "$CONTAINER_OPT_DIR/kubevirt/virtctl" https://github.com/kubevirt/kubevirt/releases/download/v${KUBEVIRT_VERSION}/virtctl-v${KUBEVIRT_VERSION}-linux-amd64
+wget -q -O "$CONTAINER_OPT_DIR/kubevirt/virtctl" "https://github.com/kubevirt/kubevirt/releases/download/v${KUBEVIRT_VERSION}/virtctl-v${KUBEVIRT_VERSION}-linux-amd64"
 chmod a+x "$CONTAINER_OPT_DIR/kubevirt/virtctl"
 rm -rf "${TMPDIR:?}"/*
 chmod -R +x "${CONTAINER_USR_BIN_DIR}"
 
 echo "Downloading kustomize ${KUSTOMIZE_VER}"
 mkdir -p "$CONTAINER_OPT_DIR/kustomize/"
-wget -q -O- https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv${KUSTOMIZE_VER}/kustomize_v${KUSTOMIZE_VER}_linux_amd64.tar.gz | \
+wget -q -O- "https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv${KUSTOMIZE_VER}/kustomize_v${KUSTOMIZE_VER}_linux_amd64.tar.gz" | \
   tar xz -C "$CONTAINER_OPT_DIR/kustomize/"
 rm -rf "${TMPDIR:?}"/*
 chmod -R +x "${CONTAINER_USR_BIN_DIR}"
