a2fe5bf79289fde9727249a42b561912a5e2e235
Angel Misevski <amisevsk@redhat.com>
2023-02-01 12:05:41 -0500

    Make CLI tools optional when generating versions; fix oc version string
    * Don't attempt to generate version numbers for CLIs that are not
      installed
    * Fix how we get oc version, as releaseClientVersion isn't always
      available
    
    Signed-off-by: Angel Misevski <amisevsk@redhat.com>

--- etc/get-tooling-versions.sh
+++ etc/get-tooling-versions.sh
@@ -11,52 +11,63 @@
 #   Red Hat, Inc. - initial API and implementation
 #
 
-OC_VER=$(oc version --client -o json | jq -r '.releaseClientVersion')
-KUBECTL_VER=$(kubectl version --client -o json | jq -r '.clientVersion.gitVersion')
+INSTALLED_TOOLS="Command |Version |Name"
+
+function append_ver() {
+  INSTALLED_TOOLS="$INSTALLED_TOOLS\n$1"
+}
+
+OC_VER=$(oc version --client -o json | jq -r '.clientVersion.gitVersion' | grep -Eo 'v?[0-9]+\.[0-9]+\.[0-9]+')
+append_ver "oc       |${OC_VER#v}        |OpenShift CLI"
+
+KUBECTL_VER=$(kubectl version --client -o json | jq -r '.clientVersion.gitVersion' | grep -Eo 'v?[0-9]+\.[0-9]+\.[0-9]+')
+append_ver "kubectl  |${KUBECTL_VER#v}   |Kubernetes CLI"
+
 if command -v kustomize &>/dev/null; then
-  KUSTOMIZE_VER=$(kustomize version --short | grep -Eo 'v[0-9]+\.[0-9]+\.[0-9]+')
+  KUSTOMIZE_VER=$(kustomize version --short | grep -Eo 'v?[0-9]+\.[0-9]+\.[0-9]+')
+  append_ver "kustomize|${KUSTOMIZE_VER#v} |Kustomize CLI"
 else
   KUSTOMIZE_VER=$(kubectl version --client -o json | jq -r '.kustomizeVersion')
-  BUILTIN_KUSTOMIZE="true"
+  append_ver "kustomize|${KUSTOMIZE_VER#v} |Kustomize CLI (built-in to kubectl)"
 fi
-HELM_VER=$(helm version --short --template '{{.Version}}' 2>/dev/null)
-KN_VER=$(kn version -o json | jq -r '.Version')
-TKN_VER=$(tkn version --component client)
-SUBMARINER_VER=$(subctl version | grep -Eo 'v[0-9]+\.[0-9]+\.[0-9]+')
-ODO_VER=$(odo version --client | grep -Eo 'v[0-9]+\.[0-9]+\.[0-9]+')
-RHOAS_VER=$(rhoas version | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+')
-KUBEVIRT_VER=$(virtctl version --client | grep -Eo 'v[0-9]+\.[0-9]+\.[0-9]+')
+
+if command -v helm &>/dev/null; then
+  HELM_VER=$(helm version --short --template '{{.Version}}' 2>/dev/null | grep -Eo 'v?[0-9]+\.[0-9]+\.[0-9]+')
+  append_ver "helm     |${HELM_VER#v}      |Helm CLI"
+fi
+
+if command -v kn &>/dev/null; then
+  KN_VER=$(kn version -o json | jq -r '.Version')
+  append_ver "kn       |${KN_VER#v}        |KNative CLI"
+fi
+
+if command -v tkn &>/dev/null; then
+  TKN_VER=$(tkn version --component client)
+  append_ver "tkn      |${TKN_VER#v}       |Tekton CLI"
+fi
+
+if command -v subctl &>/dev/null; then
+  SUBMARINER_VER=$(subctl version | grep -Eo 'v?[0-9]+\.[0-9]+\.[0-9]+')
+  append_ver "subctl   |${SUBMARINER_VER#v}|Submariner CLI"
+fi
+
+if command -v odo &>/dev/null; then
+  ODO_VER=$(odo version --client | grep -Eo 'v?[0-9]+\.[0-9]+\.[0-9]+')
+  append_ver "odo      |${ODO_VER#v}       |Red Hat OpenShift Developer CLI"
+fi
+
+if command -v virtctl &>/dev/null; then
+  KUBEVIRT_VER=$(virtctl version --client | grep -Eo 'GitVersion:"[^"]+"' | grep -Eo 'v?[0-9]+\.[0-9]+\.[0-9]+')
+  append_ver "virtctl  |${KUBEVIRT_VER#v}  |KubeVirt CLI"
+fi
+
+if command -v rhoas &>/dev/null; then
+  RHOAS_VER=$(rhoas version | grep -Eo 'v?[0-9]+\.[0-9]+\.[0-9]+')
+  append_ver "rhoas    |${RHOAS_VER#v}     |Red Hat OpenShift Application Services CLI"
+fi
+
 JQ_VER=$(jq --version)
 JQ_VER=${JQ_VER#jq-}
+append_ver "jq       |${JQ_VER#v}        |jq"
 
-if [ "$BUILTIN_KUSTOMIZE" == "true" ]; then
-  cat <<EOF | column -t -s '|'
-Command  |Version        |Name
-oc       |${OC_VER#v}        |OpenShift CLI
-kubectl  |${KUBECTL_VER#v}   |Kubernetes CLI
-kustomize|${KUSTOMIZE_VER#v} |Kustomize CLI (built-in to kubectl)
-helm     |${HELM_VER#v}      |Helm CLI
-kn       |${KN_VER#v}        |KNative CLI
-tkn      |${TKN_VER#v}       |Tekton CLI
-subctl   |${SUBMARINER_VER#v}|Submariner CLI
-odo      |${ODO_VER#v}       |Red Hat OpenShift Developer CLI
-rhoas    |${RHOAS_VER#v}     |Red Hat OpenShift Application Services CLI
-virtctl  |${KUBEVIRT_VER#v}  |KubeVirt CLI
-jq       |${JQ_VER#v}        |jq
-EOF
-else
-  cat <<EOF | column -t -s '|'
-Command  |Version        |Name
-oc       |${OC_VER#v}        |OpenShift CLI
-kubectl  |${KUBECTL_VER#v}   |Kubernetes CLI
-kustomize|${KUSTOMIZE_VER#v} |Kustomize CLI
-helm     |${HELM_VER#v}      |Helm CLI
-kn       |${KN_VER#v}        |KNative CLI
-tkn      |${TKN_VER#v}       |Tekton CLI
-subctl   |${SUBMARINER_VER#v}|Submariner CLI
-odo      |${ODO_VER#v}       |Red Hat OpenShift Developer CLI
-rhoas    |${RHOAS_VER#v}     |Red Hat OpenShift Application Services CLI
-virtctl  |${KUBEVIRT_VER#v}  |KubeVirt CLI
-jq       |${JQ_VER#v}        |jq
-EOF
-fi
+echo -e "$INSTALLED_TOOLS" | column -t -s '|'
